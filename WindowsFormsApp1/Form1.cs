8de24ea6318f95ffd56777f2d15545e7ba83a13b
Pedro Martins <pedromartins10@hotmail.com>
2022-06-16 09:42:29 -0300

    QRCode Stimulsoft

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,23 @@
+﻿using Stimulsoft.Report;
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StiReport report = new StiReport();
+            report = QReport;
+
+            report.Dictionary.Variables["Link"].Value = "https://github.com/pedromartins1035";
+            report.Show();
+        }
+    }
+}
